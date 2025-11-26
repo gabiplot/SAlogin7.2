c5b2ae933fe1554e827fd50327251b7e9cf2cca3
gabiplot <gabiplot@hotmail.com>
2025-11-25 23:54:25 -0300

    datos de la base

---
+++ migrations/usuarios.sql
@@ -0,0 +1,8 @@
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `roles`, `last_login`, `is_suspended`) VALUES
+(1, 'user', '123', '[\"ROLE_USER\"]', NULL, 0),
+(3, 'super', '123', '[\"ROLE_SUPER_ADMIN\"]', NULL, 0),
+(4, 'admin', '123', '[\"ROLE_ADMIN\"]', NULL, 0);
+
